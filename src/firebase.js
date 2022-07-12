40c6561615703a193bcc8a9eeeeccf9e0e1a1547
Adonis Edwards <adonisedwards@Adoniss-MBP.lan>
2022-07-12 10:12:06 -0400

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,10 @@
 // Import the functions you need from the SDKs you need
-import firebase from "firebase/compat/app";
-import  {getFirestore} from "firebase/firestore";
+import {getFirestore} from "firebase/firestore";
+import {initializeApp} from 'firebase/app';
 
-const firebaseApp = firebase.initializeApp({
+
+
+const firebaseApp = {
     apiKey: "AIzaSyDCLDu5J2iZNCLWCTeJqHYJwiQb3_UYSZQ",
     authDomain: "shit-5db73.firebaseapp.com",
     projectId: "shit-5db73",
@@ -10,8 +12,10 @@ const firebaseApp = firebase.initializeApp({
     messagingSenderId: "297025027097",
     appId: "1:297025027097:web:a52afb9f26ea55d1bffe88",
     measurementId: "G-HNZS6292HE",
-})
+}
 
-const db = getFirestore(firebaseApp);
+const myApp = initializeApp(firebaseApp);
+
+const db = getFirestore(myApp);
 
 export { db };
